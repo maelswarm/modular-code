f9dd0ca89765097078876deaa8f7b6af8e800018
Brandon Barber <brandonbarber@Brandons-MacBook-Pro.local>
2018-04-30 12:46:55 -0500

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = process.argv[2].slice(0, process.argv[2].lastIndexOf('/'));
+
+let parseFile = (filename, root) => {
+  let newContent = '';
+  let data = fs.readFileSync(filename, 'utf8');
+  while(data.indexOf('{{') !== -1) {
+    newContent += data.slice(0, data.indexOf('{{'));
+    data = data.slice(data.indexOf('{{'));
+    let chunk = parseFile(path + '/' + data.slice(2, data.indexOf('}}')), 0);
+    newContent += chunk.replace('\r', '');
+    data = data.slice(data.indexOf('}}') + 2);
+  }
+  newContent += data;
+  if(root) {
+    fs.writeFile(process.argv[3], newContent, (err) => {
+      if (err) throw err;
+      console.log("The file was saved!");
+    });
+  } else {
+    return newContent;
+  }
+}
+parseFile(process.argv[2], 1);
