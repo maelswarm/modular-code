388a2c69506c83c352d813dde07886836ddd85d1
Brandon Barber <brandonbarber@Brandons-MacBook-Pro.local>
2018-05-01 10:49:46 -0500

    minor

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const fs = require('fs');
-if(process.argv[2] === undefined || process.argv[3] === undefined) {console.log('Usage\nmodular-html <root module path> <output path>'); process.exit();}
+if(process.argv[2] === undefined || process.argv[3] === undefined) {console.log('Usage\nmodular-code <root module path> <output path>'); process.exit();}
 const path = process.argv[2].slice(0, process.argv[2].lastIndexOf('/'));
 
 let parseFile = (filename, root) => {
@@ -9,15 +9,14 @@ let parseFile = (filename, root) => {
   while(data.indexOf('{%') !== -1) {
     newContent += data.slice(0, data.indexOf('{%'));
     data = data.slice(data.indexOf('{%'));
-    let chunk = parseFile(path + '/' + data.slice(2, data.indexOf('%}')), 0);
-    newContent += chunk.replace(/(\r\n)+/g, '\r\n')
+    newContent += parseFile(path + '/' + data.slice(2, data.indexOf('%}')), 0);
     data = data.slice(data.indexOf('%}') + 2);
   }
   newContent += data;
   if(root) {
     fs.writeFile(process.argv[3], newContent, (err) => {
       if (err) throw err;
-      console.log("The file was saved!");
+      console.log(process.argv[3] + ' was constructed.');
     });
   } else {
     return newContent;
